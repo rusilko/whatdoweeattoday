e4caadb5d74fa3e98979e532c77a9f709aa096e5
Jakub Suder <jakub.suder@gmail.com>
2012-03-01 14:00:12 +0100

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,6 @@ set :deploy_to, "/var/www/whatdoweeattoday"
 set :use_sudo, false
 set :keep_releases, 3
 set :rails_env, "production"
-# set :rvm_ruby_string, '1.9.2'
-
-# tmp
-set :repository, "."
-set :scm, :none
-set :deploy_via, :copy
-set :copy_exclude, [".git"]
 
 server "rubydev@hydra.llp.pl", :app, :web, :db, :primary => true
 
